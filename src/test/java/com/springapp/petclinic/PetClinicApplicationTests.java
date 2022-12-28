7aef760b477d98951e59eec5de21ba80f7fb5d83
radek-nowak <radeknowak1999@gmail.com>
2022-12-28 17:22:58 +0100

    Initial commit

---
+++ src/test/java/com/springapp/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapp.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
