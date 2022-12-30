1084d271603ad35c650b5422c6c616236e64c4b3
radek-nowak <radeknowak1999@gmail.com>
2022-12-30 14:18:34 +0100

    Implement pet and vet map service

---
+++ pet-clinic-data/src/main/java/com/springapp/petclinic/service/map/PetMapService.java
@@ -0,0 +1,47 @@
+package com.springapp.petclinic.service.map;
+
+import com.springapp.petclinic.model.Owner;
+import com.springapp.petclinic.model.Pet;
+import com.springapp.petclinic.service.PetService;
+
+import java.util.Optional;
+import java.util.Set;
+
+public class PetMapService<T extends Pet, ID extends Long> extends AbstractMapService<T, ID> implements PetService<T, ID> {
+
+    @Override
+    public Set<T> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Optional<T> findById(ID id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public <S extends T> S save(S entity) {
+        return super.save(entity);
+    }
+
+    @Override
+    public void deleteById(ID id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Optional<T> findByName(String name) {
+        T pet = map.values().stream()
+                .filter(p -> p.getName().equals(name))
+                .findFirst().orElse(null);
+        return Optional.ofNullable(pet);
+    }
+
+    @Override
+    public Optional<T> findByOwner(Owner owner) {
+        T pet = map.values().stream()
+                .filter(p -> p.getOwner().equals(owner))
+                .findFirst().orElse(null);
+        return Optional.ofNullable(pet);
+    }
+}
