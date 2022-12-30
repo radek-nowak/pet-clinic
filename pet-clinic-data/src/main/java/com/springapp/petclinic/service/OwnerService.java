08e183060acc6876ee271430e937004aa695b358
radek-nowak <radeknowak1999@gmail.com>
2022-12-30 13:36:09 +0100

    Introduce owner and vet service, PersonService could implement default methods

---
+++ pet-clinic-data/src/main/java/com/springapp/petclinic/service/OwnerService.java
@@ -0,0 +1,6 @@
+package com.springapp.petclinic.service;
+
+import com.springapp.petclinic.model.Owner;
+
+public interface OwnerService extends PersonService<Owner, Long> {
+}
--- pet-clinic-data/src/main/java/com/springapp/petclinic/service/PersonService.java
+++ pet-clinic-data/src/main/java/com/springapp/petclinic/service/PersonService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 public interface PersonService<T extends Person, ID> extends CrudService<T, ID> {
 
+    // TODO: refactor as default methods.
     Optional<T> findByName(String name);
 
     Optional<T> findByLastName(String name);
