9ea56532a90dd1c45c8b3e9026efd931b36b11cd
radek-nowak <radeknowak1999@gmail.com>
2023-01-05 14:19:03 +0100

    Initial commit

---
+++ pet-clinic-data/src/main/java/com/springapp/petclinic/model/Visit.java
@@ -0,0 +1,33 @@
+package com.springapp.petclinic.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity {
+    private LocalDate date;
+    private String description;
+    private Long petId;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getPetId() {
+        return petId;
+    }
+
+    public void setPetId(Long petId) {
+        this.petId = petId;
+    }
+}
