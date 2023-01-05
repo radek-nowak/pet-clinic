bcf6c86326fcb3971d8baf266257688e7a7eb2ad
radek-nowak <radeknowak1999@gmail.com>
2023-01-05 14:48:33 +0100

    Initial commit, add attribute to vet

--- pet-clinic-data/src/main/java/com/springapp/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/springapp/petclinic/model/Vet.java
@@ -1,4 +1,13 @@
 package com.springapp.petclinic.model;
 
 public class Vet extends Person{
+    private VetSpeciality speciality;
+
+    public VetSpeciality getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(VetSpeciality speciality) {
+        this.speciality = speciality;
+    }
 }
